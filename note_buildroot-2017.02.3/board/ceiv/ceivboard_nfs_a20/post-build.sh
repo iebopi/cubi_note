694a309d2fcbfd7823d705445735614345fffe8c
iebopi <iezghui@126.com>
2017-07-14 08:44:36 +0800

    new

---
+++ note_buildroot-2017.02.3/board/ceiv/ceivboard_nfs_a20/post-build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# post-build.sh for Cubieboard2
+# 2013, Carlo Caione <carlo.caione@gmail.com>
+
+BOARD_DIR="$(dirname $0)"
+MKIMAGE=$HOST_DIR/usr/bin/mkimage
+BOOT_CMD=$BOARD_DIR/boot.cmd
+BOOT_CMD_H=$BINARIES_DIR/boot.scr
+
+# cp qt demo app
+cp $BUILD_DIR/qt-4.8.7/demos/mainwindow/mainwindow $TARGET_DIR/root/
+
+# add startup item
+sed -i '/rcqt/d' $TARGET_DIR/etc/profile
+echo "/etc/init.d/rcqt" >> $TARGET_DIR/etc/profile
+echo "/root/mainwindow -qws &" > $TARGET_DIR/etc/init.d/rcqt
+fakeroot chmod +x $TARGET_DIR/etc/init.d/rcqt
+
+# setup network dhcp
+echo "auto eth0\niface eth0 inet dhcp" >> $TARGET_DIR/etc/network/interfaces
+
+# delete getty line to auto login
+sed -i '/getty/a\::respawn:-/bin/sh' $TARGET_DIR/etc/inittab
+sed -i '/getty/d' $TARGET_DIR/etc/inittab
+
+# U-Boot script
+$MKIMAGE -C none -A arm -T script -d $BOOT_CMD $BOOT_CMD_H
