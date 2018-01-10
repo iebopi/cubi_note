99129d4b8e5e510efa1f7a9d272fd05f85a769e9
iebopi <iezghui@126.com>
2018-01-10 20:04:31 +0800

    vsftp

--- vsftp/_install/autorun.sh
+++ vsftp/_install/autorun.sh
@@ -1,5 +1,6 @@
-ifconfig eth1 10.21.1.99
-ifconfig eth0 10.21.1.98
+ifconfig eth0 192.168.1.99
+ifconfig eth1 192.168.2.99
+
 #sleep 10
 chown root:root /etc/vsftpd.conf
 mkdir -p /usr/share/empty
@@ -7,6 +8,13 @@ passwd << EOF
 root
 root
 EOF
-
 vsftpd &
+
+mkdir -p /opt/SeedingController/config
+mkdir -p /opt/SeedingController/media
+chmod 777 -R /opt/SeedingController/*
+cd /opt/SeedingController/
+#./ts_deamon &
+
 echo "done!"
+
