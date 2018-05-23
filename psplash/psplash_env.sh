3ecf12f265f66ec7b6b12ebca297428e5f3f1c86
iebopi <iezghui@126.com>
2018-05-23 00:14:07 -0700

    psplash

---
+++ psplash/psplash_env.sh
@@ -0,0 +1,6 @@
+apt-get install build-essential libncurses5-dev
+apt-get install autoconf
+apt-get install libtool
+apt-get install gettext
+apt-get install libglib2.0-dev
+apt-get install libgtk2.0-dev
