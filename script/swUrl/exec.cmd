42efffd13b6c8ad5196fe703bd2dfa344ce4d1dd
iebopi <iezghui@126.com>
2018-12-20 20:42:38 +0800

    add swUrl

---
+++ script/swUrl/exec.cmd
@@ -0,0 +1,2 @@
+#https://www.pstips.net/question/4525.html
+powershell .\swUrl.ps1 "https://www.baidu.com"
---
+++ script/swUrl/swUrl.ps1
@@ -0,0 +1,26 @@
+$args[0]
+if(($args[0] -eq $null) -or ![bool]$args[0].StartsWith("http"))
+{
+	exit
+}
+$Url = $args[0]
+$idHWND = ,0
+(New-Object -comObject Shell.Application).Windows()| where {($_.FullName -ne $null) -and ($_.FullName.toLower().EndsWith("iexplore.exe")) }| foreach { $idHWND+=$_.HWND }
+$idHWND
+Start-Process -FilePath "C:\Program Files\Internet Explorer\iexplore.exe"  -ArgumentList "-k $Url"
+
+$num = 0
+$idHWND.length
+while($num -lt $idHWND.length) 
+{
+#	Start-Sleep -Milliseconds 1000
+	if($idHWND.get($num) -eq 0)
+	{
+		"continue"
+		$num = $num + 1
+		continue
+	}
+	$idHWND.get($num)
+	(New-Object -comObject Shell.Application).Windows()| where {($_.HWND -eq $idHWND.get($num)) } | foreach { $_.quit() }
+	$num = $num + 1
+}
