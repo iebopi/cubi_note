9a322d8ba8df8953a1ef005c966d70435c4daf76
iebopi <iezghui@126.com>
2019-01-24 15:29:14 +0800

    “script”

--- script/swUrl/swUrl.ps1
+++ script/swUrl/swUrl.ps1
@@ -1,13 +1,47 @@
 $args[0]
-if(($args[0] -eq $null) -or ![bool]$args[0].StartsWith("http"))
+if(($args[0] -eq $null))
 {
 	exit
 }
+if(![bool]$args[0].StartsWith("http") -and ![bool]$args[0].StartsWith("file:"))
+{
+	exit
+}
+
+function Wait-IEReady([int]$TimeoutSeconds=10)
+{
+    $milliseconds=0
+    $maxMilliseconds = $TimeoutSeconds * 1000
+    while($Global:IEHost.Busy)
+    {
+        "11ss"
+      if($milliseconds -gt $maxMilliseconds)
+      {
+        throw 'Wait ie ready timeout.'
+      }
+      sleep -Milliseconds 100
+      $milliseconds+=100
+        
+    }
+}
+
 $Url = $args[0]
+$user = $args[1]
+$passwd = $args[2]
+$timeout = $args[3]
 $idHWND = ,0
 (New-Object -comObject Shell.Application).Windows()| where {($_.FullName -ne $null) -and ($_.FullName.toLower().EndsWith("iexplore.exe")) }| foreach { $idHWND+=$_.HWND }
 $idHWND
 Start-Process -FilePath "C:\Program Files\Internet Explorer\iexplore.exe"  -ArgumentList "-k $Url"
+$vbs = New-Object -ComObject WScript.Shell
+Wait-IEReady
+Start-Sleep -Milliseconds $timeout
+$vbs.SendKeys("{TAB}")
+$vbs.SendKeys("+") # SHIFT
+$vbs.SendKeys("$user")
+$vbs.SendKeys("{TAB}")
+$vbs.SendKeys("$passwd")
+$vbs.SendKeys("{ENTER}")
 
 $num = 0
 $idHWND.length
