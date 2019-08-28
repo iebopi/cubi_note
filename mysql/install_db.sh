187db599e57f17914853720d6baab1fe97d0b704
iebopi <iezghui@126.com>
2019-08-28 08:45:57 +0800

    Update install_db.sh

--- mysql/install_db.sh
+++ mysql/install_db.sh
@@ -3,18 +3,21 @@
 #sudo mkswap /2GB_1.swap
 #sudo swapon /2GB_1.swap
 
+PORT = 13306
+
 sudo yum -y remove mariadb*
 sudo yum -y install autoconf
 mkdir -p logs
-firewall-cmd --zone=public --add-port=13306/tcp --permanent
-# systemctl stop firewalld.service
+firewall-cmd --zone=public --add-port=$PORT/tcp --permanent
+systemctl stop firewalld.service
 ./scripts/mysql_install_db --defaults-file=my.cnf
 ./bin/mysqld_safe --defaults-file=my.cnf &
-#./bin/mysql -uroot -h127.0.0.1 -P13306
-# use mysql;
-# update user set password=password('pi1415') where user='root' and host='localhost';
-# grant all privileges on *.* to root@"%" identified by 'pi1415' with grant option;
-# flush privileges;
+./bin/mysql -uroot -h127.0.0.1 -P$PORT << EOF
+ use mysql;
+ update user set password=password('pi1415') where user='root' and host='localhost';
+ grant all privileges on *.* to root@"%" identified by 'pi1415' with grant option;
+ flush privileges;
+EOF
 
-#./bin/mysql -uroot -h127.0.0.1 -P13306 -ppi1415 < "creat_db1.sql"
+./bin/mysql -uroot -h127.0.0.1 -P$PORT -ppi1415 < "creat_db1.sql"
 
